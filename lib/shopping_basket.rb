5af41cb9834e457de79046943a13c23283768528
Jerome Gagner <jerome@brocode-inc.com>
2010-08-10 23:34:13 -0700

    some more refactorings

--- lib/shopping_basket.rb
+++ lib/shopping_basket.rb
@@ -1,9 +1,11 @@
 class ShoppingBasket < Array
   def total_tax_amount
-    self.inject(0) {|total_tax, basket_item| total_tax + basket_item.tax_amount}
+    tax_amount = self.inject(0) {|total_tax, basket_item| total_tax + basket_item.tax_amount}
+    ("%.2f" % tax_amount)
   end
 
   def total_purchase_amount
-    self.inject(0) {|total_amount, basket_item| total_amount + basket_item.total_item_amount }
+    total_amount = self.inject(0) {|total_amount, basket_item| total_amount + basket_item.total_item_amount }
+    ("%.2f" % total_amount)
   end
 end
--- spec/shopping_basket_spec.rb
+++ spec/shopping_basket_spec.rb
@@ -7,12 +7,12 @@ require 'taxable_imported_basket_item'
 
 describe ShoppingBasket do
 
-  def baskets
+  def basket_items
     #1 imported bottle of perfume at 27.99
     #1 bottle of perfume at 18.99
     #1 packet of headache pills at 9.75
     #1 box of imported chocolates at 11.25
-    @baskets ||=
+    @basket_items ||=
         [
             BasketItem.new("packet of headache pills", "9.75"),
             ImportedBasketItem.new("imported chocolates ", "11.25"),
@@ -21,25 +21,25 @@ describe ShoppingBasket do
         ]
   end
 
-  def shopping_basket(items=self.baskets)
+  def shopping_basket(items=self.basket_items)
     @shopping_basket ||= ShoppingBasket.new(items)
   end
 
 
   it "should accept and keep track of basket items" do
     shopping_basket
-    shopping_basket.should include(*self.baskets)
+    shopping_basket.should include(*self.basket_items)
   end
 
   describe "#total_tax_amount" do
     it "should total the tax from its items" do
-      shopping_basket.total_taxes.should eql 6.70
+      shopping_basket.total_tax_amount.should eql "6.70"
     end
   end
 
   describe "#total_purchase_amount" do
     it "should sum all the items including taxes" do
-      shopping_basket.total_purchase_amount.should eql 74.68
+      shopping_basket.total_purchase_amount.should eql "74.68"
     end
   end
 
